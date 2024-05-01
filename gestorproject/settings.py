7afe4bc0a436553392aaf93a2beba3db38153e20
Natalia Garnica G <49888858+ntlgrnc@users.noreply.github.com>
2024-05-01 15:35:28 -0500

    debug true

--- gestorproject/settings.py
+++ gestorproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='1234')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
